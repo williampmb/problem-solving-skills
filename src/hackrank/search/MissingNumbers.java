c22a32a1f20706b2c62756c0d112b1d6bbdc9cd1
William <William@LOL-VI>
2017-02-13 12:30:16 -0300

    HackRank: #Missing Numbers #Sheclock And Array

---
+++ src/hackrank/search/MissingNumbers.java
@@ -0,0 +1,62 @@
+package hackrank.search;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author William
+ */
+public class MissingNumbers {
+    public static void main(String[] args) throws IOException{
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedReader br = new BufferedReader(new FileReader("src/input.txt"));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] array = new int[10000];
+        int sizeA = Integer.valueOf(br.readLine());
+        String[] arrayA = br.readLine().split(" ");
+        
+        int sizeb = Integer.valueOf(br.readLine());
+        String[] arrayB = br.readLine().split(" ");
+        
+        for(int i = 0; i< sizeA ; i++){
+            int numb = Integer.parseInt(arrayA[i]);
+            array[getIndex(numb)]++;
+            int numb2 = Integer.parseInt(arrayB[i]);
+            array[getIndex(numb2)]--;
+        }
+        
+        for(int j = sizeA; j< sizeb ; j++){
+            int numb = Integer.parseInt(arrayB[j]);
+            array[getIndex(numb)]--;
+        }
+        
+        for(int m = 0; m<array.length;m++){
+            if(array[m]<0){
+                bw.write(getNumb(m)+ " ");
+            }
+        }
+        bw.write("\n");
+        bw.flush();
+        
+        
+    }
+
+    private static int getIndex(int numb) {
+        return numb-1;
+    }
+    private static int getNumb(int index) {
+        return index+1;
+    }
+}
---
+++ src/hackrank/search/SherlockAndArray.java
@@ -0,0 +1,71 @@
+package hackrank.search;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author William
+ */
+public class SherlockAndArray {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("src/input.txt"));
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(br.readLine());
+        
+        while(testCase != 0){
+            int n = Integer.parseInt(br.readLine());
+            String arrayLine = br.readLine();
+            int[] array = stringToArray(arrayLine);
+            String hasSum = sumLeftEqualsRight(array);
+            bw.write(hasSum+ "\n");
+            testCase--;
+        }
+        bw.flush();
+        
+    }
+
+    private static int[] stringToArray(String arrayLine) {
+        String[] tk = arrayLine.split(" ");
+        int[] array = new int[tk.length];
+        int i =0;
+        for(String num : tk){
+            array[i]=Integer.parseInt(num);
+            i++;
+        }
+        return array;
+    }
+
+    private static String sumLeftEqualsRight(int[] array) {
+        if(array.length ==2 ){
+            return "NO";
+        }
+        if(array.length <3 ){
+            return "YES";
+        }
+        int sumLeft = array[0], sumRight =0;
+        for(int j=2; j<array.length;j++){
+            sumRight+=array[j];
+        }
+        for(int i=1;i<array.length-1;i++){
+            if(sumLeft == sumRight){
+                return "YES";
+            }
+            sumLeft+=array[i];
+            sumRight-=array[i+1];
+        }
+        return "NO";
+    }
+}
