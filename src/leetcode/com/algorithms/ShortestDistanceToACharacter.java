4aec29183c77b4cac32826737813bbd6020e1863
W. B. <williampmb@gmail.com>
2018-11-14 22:36:05 -0300

    - 821 : Shortest Distance to a Character #leetcode

---
+++ src/leetcode/com/algorithms/ShortestDistanceToACharacter.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode.com.algorithms;
+
+/**
+ *
+ * @author William Barbosa
+ */
+public class ShortestDistanceToACharacter {
+    public int[] shortestToChar(String S, char C) {
+        int N = S.length();
+        int[] ans = new int[N];
+        int prev = Integer.MIN_VALUE / 2;
+
+        for (int i = 0; i < N; ++i) {
+            if (S.charAt(i) == C) prev = i;
+            ans[i] = i - prev;
+        }
+
+        prev = Integer.MAX_VALUE / 2;
+        for (int i = N-1; i >= 0; --i) {
+            if (S.charAt(i) == C) prev = i;
+            ans[i] = Math.min(ans[i], prev - i);
+        }
+
+        return ans;
+    }
+}
