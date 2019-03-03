e0a39b3f468972acfcd7904989979eb2c76e594d
W. B. <williampmb@gmail.com>
2019-03-03 18:52:54 -0300

    - 215: Kth Largest Element in an Array #leetcode

---
+++ src/leetcode/com/algorithms/KthLargestElementInAnArray.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode.com.algorithms;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author William Barbosa
+ */
+public class KthLargestElementInAnArray {
+    public int findKthLargest(int[] nums, int k) {
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+}
