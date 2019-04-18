1b6d2e12fe61aa760fcf642d57bd573f78ccef96
William Barbosa <williampmb@gmail.com>
2019-04-18 15:03:16 -0300

    - 806: Number of Lines To Write String #leetcode

--- src/leetcode/com/algorithms/BinaryTreePruning.java
+++ src/leetcode/com/algorithms/BinaryTreePruning.java
@@ -1,5 +1,13 @@
 package leetcode.com.algorithms;
 
-public TreeNode pruneTree(TreeNode root) {
-        
+public class BinaryTreePruning {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right= pruneTree(root.right);
+        if(root.val == 1  || root.left != null || root.right != null){
+            return root;
+        }
+        return null;
+    }
 }
---
+++ src/leetcode/com/algorithms/NumberofLinesToWriteString.java
@@ -0,0 +1,23 @@
+package leetcode.com.algorithms;
+
+public class NumberOfLinesToWriteString{
+    
+    public int[] numberOfLines(int[] widths, String S) {
+        int numb =0, lines =0;
+         
+         for(int i =0 ; i< S.length(); i++){
+             char c = S.charAt(i);
+             numb += widths[c-97];
+             if(numb > 100){
+                 lines++;
+                 numb = widths[c-97];
+             }
+         }
+
+         int[] ans = new int[2];
+         ans[1] = numb;
+         ans[0] = numb>0?lines+1:lines;
+         return ans;
+    }
+
+}
