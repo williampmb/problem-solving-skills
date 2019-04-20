d8a43b1ea17f459dd05735cb0173ff7dd6ea338f
William Barbosa <williampmb@gmail.com>
2019-04-19 23:44:36 -0300

    missing method :p

--- src/leetcode/com/algorithms/DeleteColumnsToMakeSorted.java
+++ src/leetcode/com/algorithms/DeleteColumnsToMakeSorted.java
@@ -1,5 +1,16 @@
 package leetcode.com.algorithms;
 
 public class DeleteColumnsToMakeSorted{
-    
+    public int minDeletionSize(String[] A) {
+        int cols= A[0].length(), words = A.length, count =0;
+        for(int i =0 ; i < cols;i++){
+            for(int j = 0 ; j < words-1;j++){
+                if(A[j].charAt(i)>A[j+1].charAt(i)){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
 }
