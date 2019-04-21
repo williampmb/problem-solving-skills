4539785a59c4479bf9a7b3d2d4ca20c9806a31fc
William Barbosa <williampmb@gmail.com>
2019-04-21 16:29:31 -0300

    changes in 36 #leetcode

--- src/leetcode/com/algorithms/ValidSudoku.java
+++ src/leetcode/com/algorithms/ValidSudoku.java
@@ -10,12 +10,18 @@ public class ValidSudoku {
         Set<Character>[] colSet = new HashSet[9];
         Set<Character>[] gridSet = new HashSet[9];
         //(row/3)*3 + (col/3);
-       
+       for(int i = 0 ; i < 9;i++){
+        rowSet[i] = new HashSet<>();
+        colSet[i] = new HashSet<>();
+        gridSet[i] = new HashSet<>();
+
+       }
 
         for(int i = 0 ; i <9;i++){
             for(int j = 0; j<9;j++){
                 char c = board[i][j];
                 int gridLoc = (i/3)*3 + (j/3);
+                if(c == '.')continue;
                 if(rowSet[i].contains(c) || colSet[j].contains(c) || gridSet[gridLoc].contains(c)){
                     return false;
                 }
