97b5c529b99f3ead4f6cf761dbe32d4f19f27f33
William Barbosa <williampmb@gmail.com>
2020-02-28 17:10:00 -0300

    #1315. Sum of Nodes with Even-Valued Grandparent

---
+++ src/leetcode/com/algorithms/SumofNodeswithEvenValuedGrandparent.java
@@ -0,0 +1,26 @@
+package leetcode.com.algorithms;
+
+public class SumofNodeswithEvenValuedGrandparent {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int sumEvenGrandparent(TreeNode root) {
+        int result = sumEvenGradparent(root, null, null);
+        return result;
+    }
+
+    private int sumEvenGradparent(TreeNode node, TreeNode dad, TreeNode granddad) {
+        if (node == null)
+            return 0;
+        int sum = 0;
+        if (granddad != null && granddad.val % 2 == 0) {
+            sum += granddad.val;
+        }
+
+        sum += sumEvenGradparent(node.left, node, dad);
+        sum += sumEvenGradparent(node.right, node, dad);
+        return sum;
+    }
+}
