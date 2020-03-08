071caaabaa946693e03fae91dac881748a893ad3
William Barbosa <williampmb@gmail.com>
2020-03-08 19:02:21 -0300

    FIXED: Count Negative Numbers in a Sorted Matrix

--- src/leetcode/com/algorithms/CountNegativeNumbersinaSortedMatrix.java
+++ src/leetcode/com/algorithms/CountNegativeNumbersinaSortedMatrix.java
@@ -4,9 +4,9 @@ public class CountNegativeNumbersinaSortedMatrix {
 
     public static void main(String[] args) {
 
-        // int[][] grid = new int[][] { { 2, 2, 2, 2 }, { -1, -1, -4 }, };
-        int[][] grid = new int[][] { { 4, 3, 1 }, { 3, 2, -1, -2 }, { -1, -1, -2, -3 }, };
-        // } };
+        // int[][] grid = new int[][] { { 2, 2, 2, -4 }, { -1, -1, -4, -4 }, };
+        // int[][] grid = new int[][] { { 4, 3, 1 }, { -1, -1, -2 }, { -1, -1, -2 }, };
+        int[][] grid = new int[][] { {}, };
         int numb = countNegatives(grid);
         System.out.println(numb);
     }
@@ -14,35 +14,37 @@ public class CountNegativeNumbersinaSortedMatrix {
     static public int countNegatives(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        int negatives = 0, pos = 0;
+        int negatives = 0, pos = cols - 1;
+        int posMaxNegativeNumber = pos;
         for (int i = 0; i < rows; i++) {
 
-            negatives += countNegativesRow(grid[i]);
+            pos = countNegativesRow(grid[i], pos);
+            if (pos == -1) {
+                pos = posMaxNegativeNumber;
+                negatives += cols - 1 - pos;
+            } else {
+                posMaxNegativeNumber = pos;
+                negatives += cols - pos;
+                pos = pos > 0 ? pos-- : pos;
+            }
 
         }
         return negatives;
     }
 
-    static public int countNegativesRow(int[] arr) {
-        int start = 0, size = arr.length;
-        int end = size - 1, mid = 0;
-
-        while (start < end) {
+    static public int countNegativesRow(int[] arr, int lastEnd) {
+        int start = 0, end = lastEnd, mid = 0;
+        int numbPos = -1;
+        while (start <= end) {
             mid = (start + end) / 2;
-            if (arr[mid] > 0) {
+            if (arr[mid] >= 0) {
                 start = mid + 1;
             } else {
-                if (mid == 0 || arr[mid - 1] > 0) {
-                    break;
-                } else {
-                    end = mid;
-                }
+                numbPos = mid;
+                end = mid - 1;
             }
         }
-        if (mid >= size || arr[mid] > 0) {
-            return 0;
-        }
-        return size - mid;
+        return numbPos;
     }
 }
 
