7f1b15d5aba75bd5c14d9a09607872f6b8b481a2
William Barbosa <williampmb@gmail.com>
2020-09-29 10:31:33 -0300

    Vanhack Challenge

--- src/hankerrank/VanHackEventPage/main.js
+++ src/hankerrank/VanHackEventPage/main.js
@@ -1,24 +1,119 @@
+//CONSTANT EVENT TYPE
+const meetUp = "Meet Up";
+const leap = "Leap";
+const recruitingMission = "Recruiting Mission";
+const vanHackathon = "VanHackathon";
+const premiumOnly = "PREMIUM<br/>ONLY";
+const openWebinar = "Open Webinar";
+
+//ENUM TO EVENTYPE
+EventType = Object.freeze({
+  MEETUP: meetUp,
+  LEAP: leap,
+  RECRUTING: recruitingMission,
+  VANHACKATON: vanHackathon,
+  PREMIUM_ONLY: premiumOnly,
+  OPEN4ALL: openWebinar,
+});
+
+//EVENTS TO FILL THE PAGE
+const events = [
+  {
+    title: "Premium Class Vanhack",
+    locale: "Online",
+    date: "Octuber 26, 2020",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore officiis aut, quisquam quas obcaecati ut? Perferendis, dolores assumenda.",
+    type: EventType.PREMIUM_ONLY,
+  },
+  {
+    title: "Premium Mock Interview",
+    locale: "Online",
+    date: "Octuber 30, 2020",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore officiis aut, quisquam quas obcaecati ut? Perferendis, dolores assumenda.",
+    type: EventType.PREMIUM_ONLY,
+  },
+  {
+    title: "Trial Premium Webinar",
+    locale: "Montreal",
+    date: "Octuber 25, 2020",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi inventore quia aliquid blanditiis mollitia et explicabo?",
+    type: EventType.MEETUP,
+  },
+  {
+    title: "Trial Premium Webinar",
+    locale: "Online",
+    date: "Octuber 27, 2020",
+    description:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt deleniti expedita cum nesciunt autem.",
+    type: EventType.LEAP,
+  },
+  {
+    title: "Trial Premium Webinar",
+    locale: "Toronto",
+    date: "Octuber 29, 2020",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit accusantium, tenetur exercitationem quam ullam sint aperiam aut praesentium.",
+    type: EventType.RECRUTING,
+  },
+  {
+    title: "Trial Premium Webinar",
+    locale: "Online",
+    date: "November 9, 2020",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ratione, provident vitae eum error nobis!",
+    type: EventType.VANHACKATON,
+  },
+  {
+    title: "Trial Premium Webinar",
+    locale: "Online",
+    date: "November 16, 2020",
+    description:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia aperiam est, dolores incidunt maiores officiis magnam enim beatae nulla! Enim?",
+    type: EventType.OPEN4ALL,
+  },
+];
+
+//STARTUP SCRIPT BEFORE LOAD
 function startup() {
   const listEvents = document.getElementById("events");
-  const event = createCard();
-  const event1 = createCard();
-  listEvents.appendChild(event);
-  listEvents.appendChild(event1);
-}
+  events.forEach((event) => {
+    let eventCard = createCard(event);
 
-function newElement(element, className) {
-  return document.createElement(element).classList.add(className);
+    listEvents.appendChild(eventCard);
+  });
 }
 
 function createCard(event) {
   //Create left block with calendar and date
+  const leftDiv = createLeftDivCard(event);
+
+  //Create middle block with title and some text
+  const middleDiv = createMiddleDivCard(event);
+
+  //Create right block with premium flag and
+  const cardDetails = createRightDivCard(event);
+
+  //Create and mount the event card
+
+  const card = document.createElement("DIV");
+  card.classList.add("card");
+  card.appendChild(leftDiv);
+  card.appendChild(middleDiv);
+  card.appendChild(cardDetails);
+  return card;
+}
+
+function createLeftDivCard(event) {
   const iconCalendar = document.createElement("I");
   iconCalendar.classList.add("fa");
   iconCalendar.classList.add("fa-calendar");
   const dateSpan = document.createElement("SPAN");
   dateSpan.classList.add("publication-details");
   dateSpan.classList.add("date");
-  dateSpan.innerHTML = " March 30, 2020";
+  dateSpan.innerHTML = event.date;
 
   const imageDataDiv = document.createElement("DIV");
   imageDataDiv.classList.add("background-image");
@@ -29,20 +124,31 @@ function createCard(event) {
   cardImage.classList.add("image-data");
   cardImage.appendChild(imageDataDiv);
 
-  //Create middle block with title and some text
+  return cardImage;
+}
 
+function createMiddleDivCard(event) {
   const title = document.createElement("DIV");
   title.classList.add("title");
-  title.innerHTML = "Artificial Inteligente";
+  title.innerHTML = event.title;
 
+  const iconMap = document.createElement("I");
+  iconMap.classList.add("fa");
+  iconMap.classList.add("fa-map-marker");
+  const locale = document.createElement("SPAN");
+  locale.innerHTML = event.locale;
   const subtitle = document.createElement("DIV");
   subtitle.classList.add("subtitle");
-  subtitle.innerHTML = "Ut sed metus et nunc aliquam porta";
+  subtitle.appendChild(iconMap);
+  subtitle.appendChild(locale);
 
   const description = document.createElement("DIV");
   description.classList.add("description");
-  description.innerHTML =
-    "Cras in risus nisi. Fusce feugiat diam nunc, sit amet interdum ligula vehicula vitae. Sed gravida turpis at purus volutpat";
+  let shortDesc = event.description;
+  if (shortDesc.length > 170) {
+    shortDesc = shortDesc.substring(0, 150) + "...";
+  }
+  description.innerHTML = shortDesc;
 
   const cardData = document.createElement("DIV");
   cardData.classList.add("post-data");
@@ -50,11 +156,17 @@ function createCard(event) {
   cardData.appendChild(subtitle);
   cardData.appendChild(description);
 
-  //Create right block with premium flag and details
+  return cardData;
+}
 
+function createRightDivCard(event) {
   const premiumFlag = document.createElement("SPAN");
-  premiumFlag.classList.add("premium-flag");
-  premiumFlag.innerHTML = "PREMIUM";
+  if (event.type === EventType.PREMIUM_ONLY) {
+    premiumFlag.classList.add("premium-flag");
+  } else {
+    premiumFlag.classList.add("no-premium-event");
+  }
+  premiumFlag.innerHTML = event.type;
 
   const svgPremium = document.getElementById("premium-svg");
   const clonePremium = svgPremium.cloneNode(true);
@@ -67,8 +179,12 @@ function createCard(event) {
   cta.appendChild(linkCta);
 
   const flag = document.createElement("DIV");
+  flag.classList.add("flag");
+
+  if (event.type === EventType.PREMIUM_ONLY) {
+    flag.appendChild(clonePremium);
+  }
   flag.appendChild(premiumFlag);
-  flag.appendChild(clonePremium);
 
   const cardDetails = document.createElement("DIV");
   cardDetails.classList.add("details");
@@ -76,14 +192,7 @@ function createCard(event) {
   cardDetails.appendChild(flag);
   cardDetails.appendChild(cta);
 
-  //Create and mount the event card
-
-  const card = document.createElement("DIV");
-  card.classList.add("card");
-  card.appendChild(cardImage);
-  card.appendChild(cardData);
-  card.appendChild(cardDetails);
-  return card;
+  return cardDetails;
 }
 
 startup();
