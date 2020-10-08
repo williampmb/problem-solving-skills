e25c3c35655e8369878bce88f38c50b4ab2252e2
William Barbosa <williampmb@gmail.com>
2020-10-08 19:54:51 -0300

    #1342. Number of Steps to Reduce a Number to Zero

--- src/leetcode/com/algorithms/LRUCache.java
+++ src/leetcode/com/algorithms/LRUCache.java
@@ -1,3 +1,5 @@
+package leetcode.com.algorithms;
+
 import java.util.Hashtable;
 import java.util.Map;
 
@@ -14,72 +16,73 @@ class LRUCache {
         nodes = 0;
         map = new Hashtable<>();
     }
-    
-    public int get(int key) {
-        if(!map.containsKey(key)) return -1;
-        DuplexLinkedList cur = map.get(key);
-        if(nodes == 1) return head.value;
 
-        //least
-        if(cur.next == null){
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+        DuplexLinkedList cur = map.get(key);
+        if (nodes == 1)
+            return head.value;
+
+        // least
+        if (cur.next == null) {
             tail = cur.prev;
         }
-        //not the first
-        if(cur.prev != null){
+        // not the first
+        if (cur.prev != null) {
             cur.prev.next = cur.next;
             cur.next = head;
             head = cur;
         }
         return head.value;
     }
-    
+
     public void put(int key, int value) {
         DuplexLinkedList cur;
         boolean newValue = false;
-        if(!map.containsKey(key)){
-           cur = new DuplexLinkedList(value);
-           map.put(key, cur);
-           newValue = true;
-           nodes++;
-        }else{
+        if (!map.containsKey(key)) {
+            cur = new DuplexLinkedList(value);
+            map.put(key, cur);
+            newValue = true;
+            nodes++;
+        } else {
             cur = map.get(key);
             cur.value = value;
         }
 
-        if(newValue){
+        if (newValue) {
             head.prev = cur;
             cur.next = head;
             head = cur;
-            if(tail == null){
+            if (tail == null) {
                 tail = cur;
             }
-        }else{
-            //tail and head
-            if(cur == tail && cur==head){
-         
-                    tail = tail.prev;
-            }//tail
-            else if(cur == tail) {
-                    tail = cur;
-               
-            } //middle
-            else if(cur != head){
+        } else {
+            // tail and head
+            if (cur == tail && cur == head) {
+
+                tail = tail.prev;
+            } // tail
+            else if (cur == tail) {
+                tail = cur;
+
+            } // middle
+            else if (cur != head) {
                 cur.prev.next = cur.next;
                 cur.next.prev = cur.prev;
             }
-            if(cur != head){
+            if (cur != head) {
                 cur.next = head;
                 head.prev = cur;
                 head = cur;
             }
-            
+
         }
 
-        
-        if(capacity > nodes){
-            if(tail.prev == null){
+        if (capacity > nodes) {
+            if (tail.prev == null) {
                 tail = cur;
-            }else{
+            } else {
                 tail = tail.prev;
                 tail.next = null;
             }
@@ -88,11 +91,12 @@ class LRUCache {
         }
     }
 
-    private class DuplexLinkedList{
+    private class DuplexLinkedList {
         DuplexLinkedList next;
         DuplexLinkedList prev;
         int value;
-        DuplexLinkedList(int v){
+
+        DuplexLinkedList(int v) {
             value = v;
         }
     }
---
+++ src/leetcode/com/algorithms/NumberOfStepsToReduceANumberToZero.java
@@ -0,0 +1,13 @@
+package leetcode.com.algorithms;
+
+public class NumberOfStepsToReduceANumberToZero {
+
+    public static int numberOfSteps(int num) {
+        int count = 0;
+        while (num != 0) {
+            count++;
+            num = num % 2 == 0 ? num / 2 : num - 1;
+        }
+        return count;
+    }
+}
