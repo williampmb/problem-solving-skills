76b1dafd0fccc461bc962c3e33491d1d54afbc0e
William Barbosa <wbarbosa@fullstacklabs.co>
2022-01-23 15:16:18 -0300

    CoinChange return minimum list

--- src/leetcode/com/algorithms/CoinChange.java
+++ src/leetcode/com/algorithms/CoinChange.java
@@ -5,6 +5,7 @@
  */
 package leetcode.com.algorithms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.List;
@@ -16,11 +17,20 @@ import java.util.Map;
  */
 public class CoinChange {
     public static void main(String[] args) {
-        List<Integer> coins = Arrays.asList(1,2147483647);
-        System.out.println(coinChangeTabulation(coins,2));
-        // System.out.println(coinChangeTabulation(coins,0));
-        // System.out.println(coinChangeTabulation(coins,3));
+        List<Integer> coins = Arrays.asList( 2, 5);
+        printList(coinChangeTabulationReturnMinListOfCoins(coins, 10));
+        printList(coinChangeTabulationReturnMinListOfCoins(coins, 11));
+        printList(coinChangeTabulationReturnMinListOfCoins(coins, 4));
+        printList(coinChangeTabulationReturnMinListOfCoins(coins, 7));
+        printList(coinChangeTabulationReturnMinListOfCoins(coins, 0));
 
+    }
+
+    public static void printList(List<Integer> list) {
+        for (int i : list) {
+            System.out.print(i + "-");
+        }
+        System.out.println();
     }
 
     public static int coinChangeTabulation(List<Integer> coins, int amount) {
@@ -36,7 +46,7 @@ public class CoinChange {
             for (int coin : coins) {
                 int numbComb = combinations[i] + 1;
                 int pos = i + coin;
-                if (pos<0 || pos > amount)
+                if (pos < 0 || pos > amount)
                     continue;
                 if (combinations[pos] == null || combinations[pos] > numbComb) {
                     combinations[pos] = numbComb;
@@ -45,6 +55,31 @@ public class CoinChange {
         }
 
         return combinations[amount] == null ? -1 : combinations[amount];
+    }
+
+    public static List<Integer> coinChangeTabulationReturnMinListOfCoins(List<Integer> coins, int amount) {
+        if (amount == 0)
+            return new ArrayList<>();
+        List<Integer>[] combinations = new ArrayList[amount + 1];
+        combinations[0] = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            if (combinations[i] == null)
+                continue;
+            for (int coin : coins) {
+                int newSize = combinations[i].size() + 1;
+                int pos = i + coin;
+                if (pos > amount)
+                    continue;
+                if (combinations[pos] == null || combinations[pos].size() > newSize) {
+                    combinations[pos] = new ArrayList<>();
+                    combinations[pos].addAll(combinations[i]);
+                    combinations[pos].add(coin);
+                }
+            }
+        }
+
+        return combinations[amount] == null ? new ArrayList<>() : combinations[amount];
     }
 
 }
